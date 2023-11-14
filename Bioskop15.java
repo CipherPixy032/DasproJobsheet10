3c7045f05346c1cc24d78d2f244ff08f52e7c406
CipherPixy032 <mollym3gtr@gmail.com>
2023-11-14 18:29:43 +0700

    Commit Experimen 1 nomor 5

--- Bioskop15.java
+++ Bioskop15.java
@@ -1,16 +1,26 @@
 public class Bioskop15 {
     public static void main(String[] args) {
-        String[][] penonton = new String[4][2];
-        penonton[0][0] = "Amin";
-        penonton[0][1] = "Bena";
-        penonton[1][0] = "Candra";
-        penonton[1][1] = "Dela";
-        penonton[2][0] = "Eka";
-        penonton[2][1] = "Farhan";
-        penonton[3][0] = "Gisel";
-        System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+        String[][] audience = new String[4][2];
+        audience[0][0] = "Amin";
+        audience[0][1] = "Bena";
+        audience[1][0] = "Candra";
+        audience[1][1] = "Dela";
+        audience[2][0] = "Eka";
+        audience[2][1] = "Farhan";
+        audience[3][0] = "Gisel";
+        audience[3][1] = "Hana";
+        System.out.println(audience.length);
+        for (int i = 0; i < audience.length; i++) {
+            System.out.println("Length of row " + (i + 1) + ": " + audience[i].length);
+        }
+        System.out.printf("%s \t %s\n", audience[0][0], audience[0][1]);
+        System.out.printf("%s \t %s\n", audience[1][0], audience[1][1]);
+        System.out.printf("%s \t %s\n", audience[2][0], audience[2][1]);
+        System.out.printf("%s \t %s\n", audience[3][0], audience[3][1]);
+        System.out.println(audience.length);
+        System.out.println(audience[0].length);
+        System.out.println(audience[1].length);
+        System.out.println(audience[2].length);
+        System.out.println(audience[3].length);
     }
 }
