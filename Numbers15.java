febae90cdf140912e777db74b52033e80f6fe446
CipherPixy032 <mollym3gtr@gmail.com>
2023-11-14 22:57:02 +0700

    Commit Experiment 3

--- Numbers15.java
+++ Numbers15.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class Numbers15 {
@@ -8,11 +7,12 @@ public class Numbers15 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
 
+
         for (int i = 0; i < myNumbers.length; i++){
             System.out.println(Arrays.toString(myNumbers[i]));
         }
         for (int i = 0; i < myNumbers.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);
+            System.out.println("Lenght of row-" + (i+1) + ": " + myNumbers[i].length);
         }
     }
 }
